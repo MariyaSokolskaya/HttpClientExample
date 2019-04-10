c029153901f9acfce3d08cf1a66a0c101a5cf6b5
MariyaSokolskaya <faenna@inbox.ru>
2019-04-10 17:03:52 +0700

    Address

--- app/src/main/java/com/example/admin215/httpclientexample/MainActivity.java
+++ app/src/main/java/com/example/admin215/httpclientexample/MainActivity.java
@@ -9,19 +9,14 @@ import android.widget.EditText;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText name, surname;
@@ -52,12 +47,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
             HttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost("api.openweathermap.org/data/2.5/weather?q=Krasnoyarsk");
-            List<NameValuePair> nameValuePairs = new ArrayList<>(2);
-            nameValuePairs.add(new BasicNameValuePair("name", nameStr));
-            nameValuePairs.add(new BasicNameValuePair("surname", surnameStr));
+            HttpPost httpPost = new HttpPost("http://icomms.ru/inf/meteo.php?tid=1");
+          //  List<NameValuePair> nameValuePairs = new ArrayList<>(2);
+           // nameValuePairs.add(new BasicNameValuePair("name", nameStr));
+           // nameValuePairs.add(new BasicNameValuePair("surname", surnameStr));
             try {
-                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+             //   httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
                 HttpResponse httpResponse = httpClient.execute(httpPost);
                 if(httpResponse.getStatusLine().getStatusCode() == 200){
                     HttpEntity httpEntity = httpResponse.getEntity();
